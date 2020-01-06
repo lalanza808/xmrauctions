14b27fffac16f140abe3bc2fece08c0a06b68554
lalanza808 <lance@lzatech.org>
2020-01-06 15:45:03 -0800

    add robots txt

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
+from django.shortcuts import HttpResponse
 from . import views
 
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('robots.txt', lambda x: HttpResponse("User-Agent: *\nDisallow: /", content_type="text/plain"), name="robots_file"),
     path('health/', views.health, name='health'),
     path('shipping/edit/', views.edit_shipping, name='edit_shipping'),
 ]

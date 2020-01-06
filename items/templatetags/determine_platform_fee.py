9a4d2781c83ac0756437bab3b42b365cb0e838bf
lalanza808 <lance@lzatech.org>
2020-01-06 14:43:31 -0800

    include platform fee in bid list so it's clearly shown before sale

---
+++ items/templatetags/determine_platform_fee.py
@@ -0,0 +1,11 @@
+from math import ceil
+from django import template
+from django.conf import settings
+
+
+register = template.Library()
+
+@register.filter(is_safe=True)
+def determine_platform_fee(value):
+    platform_fee_xmr = float(value * (settings.PLATFORM_FEE_PERCENT / 100))
+    return round(platform_fee_xmr, 4)

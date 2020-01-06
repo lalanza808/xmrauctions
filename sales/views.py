6106ce3e86bd22aee94aaae2248e7bc71c46a46d
lalanza808 <lance@lzatech.org>
2020-01-06 08:34:34 -0800

    improving sale view content and adding escrow address qr code

--- sales/views.py
+++ sales/views.py
@@ -1,8 +1,12 @@
+from io import BytesIO
+from base64 import b64encode
+from qrcode import make as qrcode_make
 from django.shortcuts import render, HttpResponseRedirect, reverse
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from bids.models import ItemBid
 from sales.models import ItemSale
+
 
 
 @login_required
@@ -15,8 +19,12 @@ def get_sale(request, bid_id):
         messages.error(request, "You can't view a sale you are not involved in.")
         return HttpResponseRedirect(reverse('home'))
 
+    _address_qr = BytesIO()
+    address_qr = qrcode_make(sale.escrow_address).save(_address_qr)
+
     context = {
-        'sale': sale
+        'sale': sale,
+        'qrcode': b64encode(_address_qr.getvalue()).decode()
     }
 
     return render(request, 'sales/get_sale.html', context)
